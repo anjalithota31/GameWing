6af945a0f0aa127ae4ff17e3b48f5af11ae03760
Sujitha-5 <asujitha1177@gmail.com>
2023-11-10 17:11:40 +0530

    creatdd ClubDao

--- src/main/java/com/ty/gamewing/repository/ClubRepo.java
+++ src/main/java/com/ty/gamewing/repository/ClubRepo.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ty.gamewing.dto.Club;
 
 public interface ClubRepo extends JpaRepository<Club, Integer> {
+	
+	Club findByStatus(String status);
 
 }
