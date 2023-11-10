21726905d0c8007693c65e180f5606bf2029f783
Ankit Kumar Mishra <ankitmishra9827@gmail.com>
2023-11-10 15:04:17 +0530

    Court Entity and User Entity modified

--- src/main/java/com/ty/game_wing_springboot/dto/User.java
+++ src/main/java/com/ty/game_wing_springboot/dto/User.java
@@ -1,5 +1,33 @@
 package com.ty.game_wing_springboot.dto;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
 
+@Entity
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private long phone;
+	@Column(unique = true)
+	private String email;
+	
+	private String password;
+	
+	private Role role;
+	@OneToOne
+	private Club club;
+}
+
+enum Role{
+	ADMIN,OWNER,MANAGER,CUSTOMER
 }
