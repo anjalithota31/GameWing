2a9926682e83ad2d8ce1816df5c4a4324b6da8cc
anjalithota31 <anjalithota3110@gmail.com>
2023-11-10 14:23:07 +0530

    first commit

---
+++ src/main/java/com/ty/gamewing/GameWingApplication.java
@@ -0,0 +1,13 @@
+package com.ty.gamewing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameWingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameWingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/gamewing/GameWingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.gamewing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameWingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
