4af3f4916e31d8c44aba27af94591415782c100c
Ankit Kumar Mishra <ankitmishra9827@gmail.com>
2023-11-13 14:14:38 +0530

    Booking controller created

--- src/main/java/com/ty/gamewing/controller/BookingController.java
+++ src/main/java/com/ty/gamewing/controller/BookingController.java
@@ -1,8 +1,11 @@
 package com.ty.gamewing.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,4 +31,20 @@ public class BookingController {
 	public ResponseEntity<ResponseStructure<Booking>> findBookingById(@RequestParam int id) {
 		return service.findBookingById(id);
 	}
+
+	@GetMapping("/findallbooking")
+	public ResponseEntity<ResponseStructure<List<Booking>>> findAllBooking() {
+		return service.findAllBooking();
+	}
+
+	@GetMapping("/findbookingbyuserid/{id}")
+	public ResponseEntity<ResponseStructure<Booking>> findBookingByUserId(@PathVariable int id) {
+		return service.findBookingById(id);
+	}
+
+	@GetMapping("/findbookingbyclubid")
+	public ResponseEntity<ResponseStructure<Booking>> findBookingByClubId(@RequestParam int id) {
+		return service.findBookingById(id);
+	}
+
 }
